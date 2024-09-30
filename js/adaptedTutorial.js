01e5eddaa89498daf518c5c7fe3ecaccd53e321b
Shannon Horwitz <sehorwitz@wisc.edu>
2024-09-30 19:40:21 -0400

    Sequence Interaction
    Completion of 1-2, Lesson 3 - Sequence Interaction

--- js/adaptedTutorial.js
+++ js/adaptedTutorial.js
@@ -1,61 +1,64 @@
 /* Stylesheet by Shannon Horwitz, 2024 */
 
-//declare map variable globally so all functions have access
+// Global variables
 var map;
 var minValue;
 
-//step 1 create map
+// Create function to create map
 function createMap(){
 
-    //create the map
+    // Create the map and specify display
     map = L.map('map', {
         center: [0, 0],
-        zoom: 2
+        zoom: 3
     });
 
-    //add OSM base tilelayer
+    // Create OSM base tilelayer
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    // Add tile layer to the map
     }).addTo(map);
 
-    //call getData function
+    // Call getData function with map paramteter
     getData(map);
 };
 
+// Create the calculate min value function
 function calculateMinValue(data){
-    //create empty array to store all data values
+    // Create empty array to store all data values
     var allValues = [];
-    //loop through each city
+    // Loop through each city
     for(var city of data.features){
-        //loop through each year
+        // Loop through each year
         for(var year = 1985; year <= 2015; year+=5){
-              //get population for current year
+              // Get population for current year
               var value = city.properties["Pop_"+ String(year)];
-              //add value to array
+              // Add value to array
               allValues.push(value);
         }
     }
-    //get minimum value of our array
+    // Get minimum value of array
     var minValue = Math.min(...allValues)
-
+    // Make min variable value available to other functions
     return minValue;
 }
 
-//calculate the radius of each proportional symbol
+// Calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
-    //constant factor adjusts symbol sizes evenly
+    // Constant factor adjusts symbol sizes evenly
     var minRadius = 5;
     //Flannery Apperance Compensation formula
     var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
-
+    // Make radius variable value available to other functions
     return radius;
 };
 
-function pointToLayer(feature, latlng){
-    //Determine which attribute to visualize with proportional symbols
-    var attribute = "Pop_2015";
+// Create pointToLayer function with feature, latlng, and attributes parameters
+function pointToLayer(feature, latlng, attributes){
+    // Create attribute value to access first element in attributes array
+    var attribute = attributes[0];
 
-    //create marker options
+    // Create marker options
     var options = {
         fillColor: "#ff7800",
         color: "#000",
@@ -64,38 +67,36 @@ function pointToLayer(feature, latlng){
         fillOpacity: 0.8
     };
 
-    //For each feature, determine its value for the selected attribute
+    // For each feature, determine its value for the selected attribute
     var attValue = Number(feature.properties[attribute]);
 
-    //Give each feature's circle marker a radius based on its attribute value
+    // Give each feature's circle marker a radius based on its attribute value
     options.radius = calcPropRadius(attValue);
 
-    //create circle marker layer
+    // Create circle marker layer
     var layer = L.circleMarker(latlng, options);
 
-    // //build popup content string
-    // var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p><p><b>" + attribute + ":</b> " + feature.properties[attribute] + "</p>";
-
-    //build popup content string starting with city...Example 2.1 line 24
+    // Build popup content string starting with city
     var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
 
-    //add formatted attribute to popup content string
+    // Add formatted attribute to popup content string
     var year = attribute.split("_")[1];
+
+    // Add popup content string starting with populations
     popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + " million</p>";
 
-    //bind the popup to the circle marker
-    // layer.bindPopup(popupContent);
+    // Bind the popup to the circle marker
     layer.bindPopup(popupContent, {
         offset: new L.Point(0,-options.radius) 
     });
 
-    //return the circle marker to the L.geoJson pointToLayer option
+    // Return the circle marker to the L.geoJson pointToLayer option
     return layer;
 };
 
-//Add circle markers for point features to the map
+// Create function to add circle markers for point features to the map
 function createPropSymbols(data, attributes){
-    //create a Leaflet GeoJSON layer and add it to the map
+    // Create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
         pointToLayer: function(feature, latlng){
             return pointToLayer(feature, latlng, attributes);
@@ -103,18 +104,120 @@ function createPropSymbols(data, attributes){
     }).addTo(map);
 };
 
-//Step 2: Import GeoJSON data
-function getData(){
-    //load the data
+// Create function to update proportional symbols
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            if (layer.feature && layer.feature.properties[attribute]){
+                // Access feature properties
+                var props = layer.feature.properties;
+    
+                // Update each feature's radius based on new attribute values
+                var radius = calcPropRadius(props[attribute]);
+                layer.setRadius(radius);
+    
+                // Add city to popup content string
+                var popupContent = "<p><b>City:</b> " + props.City + "</p>";
+    
+                // Add formatted attribute to panel content string
+                var year = attribute.split("_")[1];
+                popupContent += "<p><b>Population in " + year + ":</b> " + props[attribute] + " million</p>";
+    
+                // Update popup content            
+                popup = layer.getPopup();            
+                popup.setContent(popupContent).update();
+            };
+        };
+    });
+};
+
+// Create new sequence controls
+function createSequenceControls(attributes){
+    
+    //Create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+    
+
+    //set slider attributes
+    document.querySelector(".range-slider").max = 6;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse"></button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward"></button>');
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.png'>");
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>")
+    
+
+    // document.querySelectorAll('.step').forEach(function(step){
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+            
+            // Increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                // If past the last attribute, wrap around to first attribute
+                index = index > 6 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //If past the first attribute, wrap around to last attribute
+                index = index < 0 ? 6 : index;
+            };
+            
+            // Update slider
+            document.querySelector('.range-slider').value = index;
+            updatePropSymbols(attributes[index]);
+            
+
+        })
+        
+    })
+    
+};
+
+// Create function to process data
+function processData(data){
+    
+    // Create an empty array to hold attributes
+    var attributes = [];
+
+    // Properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    // Push each attribute name into attributes array
+    for (var attribute in properties){
+        
+        // Only take attributes with population values
+        if (attribute.indexOf("Pop") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    // Make attributes variable available to other functions
+    return attributes;
+};
+
+// Import GeoJSON data
+function getData(map){
+    
+    // Load the data
     fetch("data/MegaCities.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
-            //calculate minimum data value
+            
+            // Create an attributes array
+            var attributes = processData(json);
+            
+            // Calculate minimum data value
             minValue = calculateMinValue(json);
-            //call function to create proportional symbols
-            createPropSymbols(json);
+            
+            // Call function to create proportional symbols
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
         })
 };
 
